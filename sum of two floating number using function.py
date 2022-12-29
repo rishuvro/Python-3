d940e1855a3dc5da3e57980eea08c0923ae70a36
RAKIBUL ISLAM <86145171+rishuvro@users.noreply.github.com>
2022-12-29 11:35:46 +0600

    Create sum of two floating number using function.py

---
+++ sum of two floating number using function.py
@@ -0,0 +1,12 @@
+'''
+ write a Python program that takes two floating number from the user and
+find the sum of the number using Function.
+'''
+
+def sum(a,b):
+  result = a+b
+  print(a,"+",b,"=",result)
+x = float(input('Enter first number: '))
+y = float(input('Enter second number: '))
+
+sum(x,y)
